f4d8f2cd3feafa1e851d4f3803bfd7e8ff27ad2c
waritta <waritta@128.237.223.120>
2014-01-20 13:50:08 -0500

    loginform

--- Task7/src/com/cfs/formbean/LoginForm.java
+++ Task7/src/com/cfs/formbean/LoginForm.java
@@ -1,30 +1,60 @@
 package com.cfs.formbean;
 
+import java.util.ArrayList;
+
 import org.mybeans.form.FormBean;
 
 public class LoginForm extends FormBean{
-
 	private String username;
 	private String password;
-	private String type;
+	private String accountType;
+	private String action;
+	
 	
 	public String getUsername() {
 		return username;
 	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
+	
 	public String getPassword() {
 		return password;
 	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
+	
+	public String getAccountType() {
+		return accountType;
 	}
 	
+	public String getAction() {
+		return action;
+	}
+	
+	public void setUsername(String e) { 
+		username = trimAndConvert(e, "<>\""); 
+	}
+    
+	public void setPassword(String p) { 
+		password = trimAndConvert(p, "<>\""); 
+	}
+    
+	public void setAccountType(String a) {
+		accountType = a;
+	}
+	
+	public void setAction(String a) { 
+		action   = a;        
+	}
+	
+    public ArrayList<String> getValidationErrors() {
+        ArrayList<String> errors = new ArrayList<String>();
+
+        if (username == null || username.length() == 0) {
+        	errors.add("Username is required");
+        }
+        
+        if (password == null || password.length() == 0) {
+        	errors.add("Password is required");
+        }
+
+        return errors;
+    }
 }
+
